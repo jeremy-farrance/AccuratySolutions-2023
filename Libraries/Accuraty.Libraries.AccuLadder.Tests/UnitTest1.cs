497cd75bd35a1c4121a6de62e42e7f87e9c5bc6a
Jeremy Farrance <jfarrance@accuraty.com>
2023-10-28 20:20:37 -0500

    Initial experiment to see if we can easily start testing this

---
+++ Libraries/Accuraty.Libraries.AccuLadder.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DotNetNuke.Entities.Users;
+
+namespace Accuraty.Libraries.AccuLadder.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void IsAdminOrSuper_ReturnsTrue_IfUserIsSuperUser()
+    {
+      // Arrange
+      var userInfo = new UserInfo
+      {
+        IsSuperUser = true,
+      };
+
+      // Act
+      bool result = Accu.Dnn.IsAdminOrSuper(userInfo);
+
+      // Assert
+      Assert.IsTrue(result);
+    }
+  }
+}
