86089183c03da32b159a2b46f2bd904cb24c8dc2
Jeremy Farrance <jfarrance@accuraty.com>
2023-11-06 21:41:49 -0600

    Added IconHelper (unfinished) and a few small enhancements/fixes

---
+++ Libraries/AccuLadder/Accu/DnnHelpers.cs
@@ -0,0 +1,30 @@
+using DotNetNuke.Entities.Modules;  // ModuleInfo
+using DotNetNuke.Entities.Portals;  // PortalSettings // [SiteInfo]
+using DotNetNuke.Entities.Tabs;     // TabInfo // [PageInfo]
+using DotNetNuke.Entities.Users;    // UserInfo
+
+// Note that initially we are planning to write this in a way that it can replace all 
+// previous versions of AccuKit (RazorKit, AccuKit11, etc.) with no errors. This may 
+// result in a LOT of code that is not needed, but it will be a good starting point.
+
+namespace Accuraty.Libraries.AccuLadder
+{
+  /// <summary>
+  /// Root for calling AccuLadder Helpers; Accu.Dev, .Dnn, etc. - Tools, utilities, and shortcuts for DNN and 2SC projects
+  /// </summary>
+  public partial class Accu
+  {
+    /// <summary>
+    /// Accu.Dnn Helper - Tools, utilities, and shortcuts for DNN-specific things
+    /// </summary>
+    public partial class Dnn
+    {
+      /// <summary>
+      /// This is where we were thinking of instantiating the Dnn Helper for Portal/Site, Page/Tab, Module, User.
+      /// </summary>
+
+
+    }
+
+  } // class Accu
+}
